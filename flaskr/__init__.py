b2d0c9d9f15b8475e30a26b5c7b25d6cd015273c
weijiayun <jiayun.wei@foxmail.com>
2016-05-21 16:42:07 +0800

    gunicorn

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from werkzeug.contrib.fixers import ProxyFix
 from flask.ext.sqlalchemy import SQLAlchemy
 from flask.ext.login import LoginManager
 import re
 from flask.ext.mail import Mail
 from momentjs import momentjs
+
 app=Flask(__name__)
+app.wsgi_app=ProxyFix(app.wsgi_app)
 app.config.from_object('config')
 
 ################mail setting#####
